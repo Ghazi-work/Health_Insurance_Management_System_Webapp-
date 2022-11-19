f1b1f878c7e0250720181e7e3a8d19b393fc4298
Syed Ghazi Ali Zaidi <94217827+Ghazi-work@users.noreply.github.com>
2022-11-19 20:51:44 +0500

    Initializing project

---
+++ Health_Insuarance_Management_System_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Health_Insuarance_Management_System_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
